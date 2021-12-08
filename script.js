25b71952798d5a749fb3fdb53cbabb42d7d90870
Noyalbabu <74628909+Noyalbabu@users.noreply.github.com>
2021-12-08 14:07:11 +0000

    Forms

---
+++ script.js
@@ -0,0 +1,16 @@
+
+ function validateForm()
+ { 
+    
+    var re = /^[A-Za-z]{2,}$/;
+    var checkForename = document.getElementById("frmForename").value;
+    var checkSurname = document.getElementById("frmSurname");
+    if(re.test(checkForename) && (re.test(check))){
+        return true;
+    }
+    else if()
+    {
+        document.getElementById("errormsg").innerHTML= "Enter a valid name";
+        return false;
+    }
+ }
