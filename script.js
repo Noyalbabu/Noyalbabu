12d6c752ef2c5f4e062564143dfa4fd90e857634
Noyalbabu <74628909+Noyalbabu@users.noreply.github.com>
2021-12-09 21:33:59 +0000

    ongoing validation

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ var forename = document.getElementById("frmForename");
 var surname = document.getElementById("frmSurname");
 var gender = document.getElementById("frmGender");
 var dob = document.getElementById("frmDOB");
-var age = document.getElementById("age");
+var age = document.getElementById("frmage");
 var course = document.getElementById("frmCourse");
 var registerdate = document.getElementById("frmDateReg");
-
+var re = /^[A-Za-z]{2,}$/;
+var checkAge = /^[12-150]$/;
 function validateInput(){
-    var re= /^[A-Za-z]{2,}$/;
     
-    if(re.test(forename)){
-
+    if(re.test(forename.value.trim())){
+        success(forename);      
     }
+    else{
+        fail(forename,"Enter a valid name");
+    }
+
+    if(re.test(surname.value.trim())){
+        success(surname);
+    }
+    else{
+        fail(surname,"Enter a valid name");
+    }
+    
+    if(checkAge.test(age.value.trim())){
+        success(age);
+    }else{
+        fail(age,"You are not eligible");
+    }
+    
 }
 
-document.getElementById("button")
+document.querySelector("button")
 .addEventListener("click",(event)=>{
     event.preventDefault();
     validateInput();
 });
+
+function success(input){
+    let parent = input.parentElement;
+    let errorMsg = parent.querySelector("small");
+    errorMsg.style.visibility = "hidden";
+    errorMsg.innerHTML ="";  
+}
+function fail(input,msg){
+    let parent = input.parentElement;
+    let errorMsg = parent.querySelector("small");
+    errorMsg.style.visibility = "visible";
+    errorMsg.innerHTML = msg;  
+}
