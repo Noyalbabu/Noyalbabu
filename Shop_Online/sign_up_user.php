65e0a3c4dc1b91fc79338057fac4826e5c2610b7
Noyalbabu <20209749@hope.ac.uk>
2022-03-04 23:05:50 +0000

    added category
    added category

--- Shop_Online/sign_up_user.php
+++ Shop_Online/sign_up_user.php
@@ -1,26 +1,47 @@
 <?php
+    $success = false;
 	$conn = mysqli_connect("localhost", "root", "root", "shopping_cart");
-
+    if (!$conn){
+		echo  "<p>Failed to connect to the TK Groups database</p>";
+	}
+	else{
 
 // enter the details of the user to a table
 		$username = $_POST["username"];
 		$address = $_POST["address"];
-        $success = false;
+        $success = true;
+	}
 
-		$sql = "INSERT INTO user_details (username, user_address)
-				VALUES('$username', '$address')";
 
-		if (mysqli_query($conn, $sql)) $success = true;
 
 ?>
 <html>
    <head>
 <title> Tk Groups </title>
+<link href="style.css" type="text/css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <body >
-    <?php
-    if ($success) echo "Welcome $username ! Hope you enjoy shopping with us.";
-    ?>
-   
+<div class = "container">
+	<div class = "main">
+	  <div class = "navigation-box">
+		<div class = "icon">
+			<i class = "fa fa-search" aria-hidden = "true">Category</i>
+        </div>
+		<div class= "content">
+			<a href = "">Women</a><br>
+			<a href = "">Mens</a><br>
+			<a href = "">Kids</a><br>
+			<a href = "">Home accessories</a><br>
+        </div>
+       </div>
+
+	  <div class = "footer">
+		<?php
+		if ($success) echo "<p><b>Welcome $username ! Hope you enjoy shopping with us.</b></p>";
+		?>
+        </div>
+    </div>
+</div> 
 <body>
 </head>
 </html>
